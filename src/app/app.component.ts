79773433200ae525ab3e8fb43f4a60938a3fc02a
Nicolás Salerno <amplificodora04@gmail.com>
2024-09-14 17:34:38 -0300

    Made angular 'event' component. Added some sample images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { EventComponent } from './event/event.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, EventComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
