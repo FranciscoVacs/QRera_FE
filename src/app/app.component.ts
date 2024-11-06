0c9c4fab7d98b2358a19475d07980fcbc6abcdcb
Nicolás Salerno <amplificodora04@gmail.com>
2024-11-06 08:37:31 -0300

    Adding a better nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { JsonPipe, CommonModule } from '@angular/common';
 import { EventPreviewComponent } from './event-preview/event-preview.component';
 import { ApiService } from './api.service.js';
 import { HomeComponent } from './home/home.component.js';
+import {MatTabsModule} from '@angular/material/tabs'; 
+import {MatButtonModule} from '@angular/material/button'; 
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, EventPreviewComponent, JsonPipe, HomeComponent, RouterLink],
+  imports: [RouterOutlet, CommonModule, EventPreviewComponent, JsonPipe, HomeComponent, RouterLink, MatTabsModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
